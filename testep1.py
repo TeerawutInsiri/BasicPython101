1355ab4a7eacaf14b552be1631367875066eb890
TeerawutInsiri <125626617+TeerawutInsiri@users.noreply.github.com>
2023-02-17 14:17:33 +0700

    การบ้าน Ep.1
    ฝึกเขียนโปรแกรมควบคุม Turtle

---
+++ testep1.py
@@ -0,0 +1,13 @@
+#Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+#Type "help", "copyright", "credits" or "license()" for more information.
+
+import turtle
+tao=turtle.Pen()
+tao.shape('turtle')
+for i in range(16):
+	tao.circle(100)
+	tao.left(22.5)
+
+	
+
+
